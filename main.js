010afa809ecabb9fc93ff1d90f880445212da6c5
gghhs19 <softtopik9999@gmail.com>
2025-05-24 05:06:06 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('startBtn').addEventListener('click', function() {
-    const loading = document.getElementById('loading');
-    loading.style.display = 'flex';
-    
-    // Анимация загрузки на 3 секунды
-    setTimeout(() => {
-        window.location.href = 'game.html';
-    }, 3000);
-});
